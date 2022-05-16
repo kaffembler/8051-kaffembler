91e400ed9e4a8e5eed6efc258ce73a6d392ecde6
nkilders <noah.kilders@gmail.com>
2022-05-16 15:24:53 +0200

    Clean up + Documentation

--- Kaffembler.asm
+++ Kaffembler.asm
@@ -22,21 +22,36 @@ TH  	 EQU TEMP.5
 
 SETB CLK
 
+
+; Main loop
 LOOP:
+  ; Tell DS1620 to send current temperature
   CALL SEND
+  ; Read data from DS1620
   CALL RECEIVE
+
+  ; Turn on the "Ready" LED if TH is high
   CALL CHECK_READY
+  ; Turn on the "Heating" LED if TL is high
   CALL CHECK_COLD
+
   JMP LOOP
 
-; Request Temperature (AAh = 10101010b)
+
+
+;-----------------------------------------------
+; Serial stuff
+;-----------------------------------------------
+
+; Tells DS1620 to send current temperature
+; Command: AAh = 10101010b
 SEND:
   ; Reset Thermometer
   CLR RST
   NOP
   SETB RST
 
-  ; Send data to DS1620
+  ; Send data
   CALL SEND_0
   CALL SEND_1
   CALL SEND_0
@@ -51,41 +66,49 @@ SEND:
 
   RET
 
-RECEIVE:
-  ; Clear A
-  MOV A, #00h
 
-  ; Pull DQ high
-  SETB DQ
-
-  ; Pulse clock
-  CALL CLOCK
-  CALL CLOCK
-  CALL CLOCK
-  CALL CLOCK
-  CALL CLOCK
-  CALL CLOCK
-  CALL CLOCK
-  CALL CLOCK
-  CALL CLOCK
-
-  CALL SHOW
-
-  RET
-
+; Sends a "1" bit over the serial port
 SEND_1:
   CLR CLK
   SETB DQ
   SETB CLK
   RET
 
+; Sends a "0" bit over the serial port
 SEND_0:
   CLR CLK
   CLR DQ
   SETB CLK
   RET
 
-CLOCK:
+
+; Reads 9 bits from the serial port
+; and shifts them into A
+RECEIVE:
+  ; Clear A
+  MOV A, #00h
+
+  ; Pull DQ high (DS1620 can only pull its data pin low when sending data)
+  SETB DQ
+
+  ; Read 9 bits from the serial port
+  CALL READ_BIT
+  CALL READ_BIT
+  CALL READ_BIT
+  CALL READ_BIT
+  CALL READ_BIT
+  CALL READ_BIT
+  CALL READ_BIT
+  CALL READ_BIT
+  CALL READ_BIT
+
+  CALL SHOW
+
+  RET
+
+
+; Reads a bit from the serial port and shifts it into A
+READ_BIT:
   CLR CLK
   JB DQ, SET_CARRY
   CALL SHIFT_AND_CLOCK_HIGH
@@ -101,10 +124,16 @@ SHIFT_AND_CLOCK_HIGH:
   RRC A
   RET
 
+
+
+; Converts the number stored in A to the corresponding bits
+; needed to display the number on the 7 segment display and
+; writes them into R2-R4
 SHOW:
   MOV B, #0Ah
   DIV AB
-  ; Wert von B auf rechte 7-Segment Anzeige  
+
+  ; write 1-digit in R2
   MOV P0, #11111111b
   CALL SHOW_NUMBER
   MOV R2, B
@@ -112,14 +141,16 @@ SHOW:
 
   MOV B, #0Ah
   DIV AB
-  ; Wert von B auf mittlere 7-Segment Anzeige
+
+  ; write 10-digit in R3
   MOV P0, #11111111b
   CALL SHOW_NUMBER
   MOV R3, B
   MOV P0, #11111101b
 
   MOV B, A
-  ; Wert von B auf linke 7-Segment Anzeige
+
+  ; write 100-digit in R4
   MOV P0, #11111111b
   CALL SHOW_NUMBER
   MOV R4, B
@@ -127,15 +158,25 @@ SHOW:
 
   RET
 
+
+; takes a value 0-9 from B, converts it to the bits needed to display
+; the number on the 7 segment display and writes them to P1
 SHOW_NUMBER:
-  ; B auflösen auf Port 1
   XCH A, B
+
+  ; convert number to corresponding bits
   MOV DPTR, #TABLE
   MOVC A, @A+DPTR
+
+  ; write bits to port 1
   MOV P1, A
+
   XCH A, B
+  
   RET
 
+
+; Refreshes the 7 segment display
 REFRESH:
   ; Wert von B auf rechte 7-Segment Anzeige
   MOV P0, #11111111b
@@ -153,6 +194,12 @@ REFRESH:
   MOV P0, #11111011b
 
   RET
+
+
+
+;-----------------------------------------------
+; Status-LEDs
+;-----------------------------------------------
 
 CHECK_READY:
   MOV A, P3
